3c2369adf8c817dfb36ef25bfce37ee1f4607a5e
eddiesosera <21100419@virtualwindow.co.za>
2023-08-01 02:25:18 +0200

    ui dependencies 1.2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import "./App.css";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faHouse, faFutbol, faCodeCompare, faCircleHalfStroke, faChartLine } from "@fortawesome/free-solid-svg-icons";
+// import anime from 'animejs/lib/anime.es.js';
+// import Swiper JS
+// import Swiper from 'swiper';
+// import 'swiper/css';
 
 function App() {
   return <div className="App">Hello World</div>;
