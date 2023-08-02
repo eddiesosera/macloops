2f8c9c109cf521211ce61b16b87a29c7f265a107
eddiesosera <21100419@virtualwindow.co.za>
2023-08-02 17:15:11 +0200

    Card params

--- src/elements/components/card.js
+++ src/elements/components/card.js
@@ -3,8 +3,12 @@ import React from "react";
 export const Card = ({ img, title, type, price, star, linkToItem }) => {
   return (
     <div>
-      <img src={img} alt={img} />
-      <div className="meta" />
+      <img src={img} alt={img} style={{ height: "300px", width: "300px" }} />
+      <div className="meta">
+        <div>{title}</div>
+        <div>{type}</div>
+        <div>{price}</div>
+      </div>
     </div>
   );
 };
--- src/elements/sections/home_sale.js
+++ src/elements/sections/home_sale.js
@@ -1,10 +1,40 @@
 import React from "react";
 import { Card } from "../components/card";
 
+import img1 from "../../img/hero/1.png";
+import img2 from "../../img/hero/2.png";
+import img3 from "../../img/hero/3.png";
+import img4 from "../../img/hero/4.png";
+
 export const Sale = () => {
+  const sale_content = [
+    {
+      imgsrc: img1,
+      buttonTitle: "Buy Now",
+      heading: "State of the Art Grand Piano"
+    },
+    {
+      imgsrc: img2,
+      buttonTitle: "Buy Now",
+      heading: "Guitar "
+    },
+    {
+      imgsrc: img3,
+      buttonTitle: "View Microphones",
+      heading: "Guitar "
+    },
+    {
+      imgsrc: img4,
+      buttonTitle: "Buy Now",
+      heading: "Guitar "
+    }
+  ];
+
   return (
-    <div>
-      <Card />
+    <div style={{ display: "flex" }}>
+      {sale_content.map(sale => {
+        return <Card img={sale.imgsrc} title={sale.heading} />;
+      })}
     </div>
   );
 };
